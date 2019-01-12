a69452487ef5b3d9dea9d4b77537d82b39dea426
lukang-local-user1 <lukang-local-user1@example.com>
2019-01-12 21:25:49 +0800

    22222

---
+++ src/components/app1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{greeting}} World!</p>
+    <other-h2></other-h2>
+  </div>
+</template>
+
+<script>
+  import app2 from "./app2.vue"
+  export default{
+    data: function(){
+      return {greeting: "Hello"}
+    },
+    components:{'other-h2':app2}
+  }
+</script>
+
+<style scoped>
+  p{
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
---
+++ src/components/app2.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>{{message}}</h2>
+</template>
+<script>
+  export default{
+    data: function(){
+      return {message:"this is app2.vue"}
+    }
+  }
+</script>
+<style scoped>
+  h2{
+    font-size: 18px;
+  }
+</style>
